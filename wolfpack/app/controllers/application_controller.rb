ad968c0609e80d6953031acd85f7bcc4a2879ff9
Stephen Sayage <stephensayage@gmail.com>
2020-12-02 15:20:11 -0500

    post request working for chat messages

--- wolfpack/app/controllers/application_controller.rb
+++ wolfpack/app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   def require_user
     if !logged_in?
-     flash[:error] = "You must be logged in to send a message"
+     flash[:error] = "You must be logged in"
      redirect_to login_path
     end
   end
