9455480b53e9686aa032589ef0ca4e0186885b91
Stephen Sayage <stephensayage@gmail.com>
2020-12-14 11:17:33 -0500

    secret key added for deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
+  SECRET_KEY = Rails.env == 'production' ? ENV['SECRET_KEY'] : Rails.application.secrets.secret_key_base.to_s
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
